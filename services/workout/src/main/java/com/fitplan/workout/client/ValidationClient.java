acc55dcf084103107031b39954d30359a0cec320
Nik <0nikjukic0@gmail.com>
2024-12-09 22:35:54 +0100

    openapi document generation with swagger

--- services/workout/src/main/java/com/fitplan/workout/client/ValidationClient.java
+++ services/workout/src/main/java/com/fitplan/workout/client/ValidationClient.java
@@ -1,15 +1,16 @@
 package com.fitplan.workout.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
 
-@FeignClient(name = "validation", url = "${validation.url}")
+//@FeignClient(name = "validation", url = "${validation.url}")
+@HttpExchange("/api/validation")
 public interface ValidationClient {
 
     //Feign client samodejno generira HTTP zahteve na podlagi tega vmesnika
-    @RequestMapping(method = RequestMethod.POST, value = "/api/validation")
+    //@RequestMapping(method = RequestMethod.POST, value = "/api/validation")
+    @PostExchange
     String askAI(@RequestParam String name, @RequestParam String description);
 
 

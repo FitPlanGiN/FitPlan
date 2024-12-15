40561f2880397a2dbf00d68c0199e8e8971e37e6
Nik <0nikjukic0@gmail.com>
2024-12-15 15:33:00 +0100

    small configs for kafka

--- services/workout/src/main/java/com/fitplan/workout/dto/WorkoutRequest.java
+++ services/workout/src/main/java/com/fitplan/workout/dto/WorkoutRequest.java
@@ -1,4 +1,7 @@
 package com.fitplan.workout.dto;
 
-public record WorkoutRequest(Long id, String name, String description) {
+public record WorkoutRequest(Long id, String name, String description, UserDetails userDetails) {
+
+    public record UserDetails(String email, String firstName, String lastName) {
+    }
 }
--- services/workout/src/main/java/com/fitplan/workout/service/WorkoutService.java
+++ services/workout/src/main/java/com/fitplan/workout/service/WorkoutService.java
@@ -2,22 +2,25 @@ package com.fitplan.workout.service;
 
 import com.fitplan.workout.client.ValidationClient;
 import com.fitplan.workout.dto.WorkoutRequest;
+import com.fitplan.workout.event.WorkoutCreatedEvent;
 import com.fitplan.workout.model.Workout;
 import com.fitplan.workout.repository.WorkoutRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
+//nevem ce slf4j dela, ampak rabmo za kreirat logger
+@Slf4j
 public class WorkoutService {
 
     private final WorkoutRepository workoutRepository;
-
     private final ValidationClient validationClient;
+    //key is topic name, value is object workoutCreatedEvent
+    private final KafkaTemplate<String, WorkoutCreatedEvent> kafkaTemplate;
 
-    public WorkoutService(WorkoutRepository workoutRepository, ValidationClient validationClient) {
-        this.workoutRepository = workoutRepository;
-        this.validationClient = validationClient;
-    }
 
     public String createWorkout(WorkoutRequest workoutRequest){
         String answerAI = validationClient.askAI(workoutRequest.name(), workoutRequest.description());
@@ -26,6 +29,12 @@ public class WorkoutService {
         workout.setName(workoutRequest.name());
         workout.setDescription(workoutRequest.description());
         workoutRepository.save(workout);
+        //send message to Kafka Topic with id, email
+        WorkoutCreatedEvent workoutCreatedEvent = new WorkoutCreatedEvent(workout.getId(), workoutRequest.userDetails().email());
+        log.info("Start - Workout created event sent to Kafka topic workout-created: {}", workoutCreatedEvent);
+        kafkaTemplate.send("workout-created", workoutCreatedEvent);
+        log.info("End - Workout created event sent to Kafka topic workout-created: {}", workoutCreatedEvent);
+
         //tukaj bo treba povezati exercise z workout z vmesno tabelo v MySQL
         return answerAI;
     }

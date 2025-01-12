dab4cc4dd066ee31a026c439a07613803805177c
Nik <0nikjukic0@gmail.com>
2025-01-12 12:48:12 +0100

    ingress

--- fitplanfrontend/src/app/services/exercise/exercise.service.ts
+++ fitplanfrontend/src/app/services/exercise/exercise.service.ts
@@ -1,8 +1,7 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
-import { catchError, switchMap } from 'rxjs/operators';
-import { Exercise } from '../../model/exercise';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Exercise} from "../../model/exercise";
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +10,6 @@ export class ExerciseService {
 
   constructor(private httpClient: HttpClient) {
   }
-
-  private clusterUrl = 'http://gateway.default.svc.cluster.local:9000/api/exercise';
-  private localUrl = 'http://localhost:9000/api/exercise';
 
   /*
   getExercises(): Observable<Array<Exercise>> {
@@ -33,29 +29,11 @@ export class ExerciseService {
     return this.httpClient.post<Exercise>('http://34.118.122.132:31823/api/exercise', exercise);
   }*/
 
-  /*getExercises(): Observable<Array<Exercise>> {
+  getExercises(): Observable<Array<Exercise>> {
     return this.httpClient.get<Array<Exercise>>('http://localhost:9000/api/exercise');
   }
 
   createExercise(exercise: Exercise): Observable<Exercise> {
     return this.httpClient.post<Exercise>('http://localhost:9000/api/exercise', exercise);
-  }*/
-
-  getExercises(): Observable<Array<Exercise>> {
-    return this.httpClient.get<Array<Exercise>>(this.clusterUrl).pipe(
-      catchError(() => {
-        console.warn('Failed to connect to cluster URL. Falling back to localhost...');
-        return this.httpClient.get<Array<Exercise>>(this.localUrl);
-      })
-    );
-  }
-
-  createExercise(exercise: Exercise): Observable<Exercise> {
-    return this.httpClient.post<Exercise>(this.clusterUrl, exercise).pipe(
-      catchError(() => {
-        console.warn('Failed to connect to cluster URL. Falling back to localhost...');
-        return this.httpClient.post<Exercise>(this.localUrl, exercise);
-      })
-    );
   }
 }
